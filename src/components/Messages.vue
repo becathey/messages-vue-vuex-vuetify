24c1280e4889ee02e37aff870b0efdc8fdba331c
Bill Cathey <billcathey@hotmail.com>
2022-05-01 01:03:43 -0400

    Create basic app, add Messages component

---
+++ src/components/Messages.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h4 class="display-1">Messages</h4>
+        
+        <v-list>
+      <v-list-item
+        v-for="(message, index) in messages" :key="index"
+      >
+
+        <v-list-item-content>
+          <v-list-item-title v-text="message"></v-list-item-title>
+        </v-list-item-content>
+
+      </v-list-item>
+    </v-list>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            messages: ["hello", "hi", "hey"]
+        }
+    },
+}
+</script>
