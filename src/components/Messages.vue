4f8edd8b5df991fec25ab2d02b9cd7d5b940e54f
Bill Cathey <billcathey@hotmail.com>
2022-05-01 16:38:46 -0400

    Set up axios to get data from backend

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -17,11 +17,15 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     data() {
         return {
             messages: ["hello", "hi", "hey"]
         }
     },
+    async created() {
+        this.messages = (await axios.get('http://localhost:3000/messages')).data
+    }
 }
 </script>
