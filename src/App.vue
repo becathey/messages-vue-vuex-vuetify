032ba007c94bcb03471a5dc81d18f567636c53ec
Bill Cathey <billcathey@hotmail.com>
2022-07-21 16:50:26 -0400

    Delete unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -21,19 +21,7 @@
 </template>
 
 <script>
-// import Messages from './components/Messages';
-// import NewMessage from './components/NewMessage'
-
 export default {
   name: 'App',
-
-  components: {
-    // Messages,
-    // NewMessage,
-  },
-
-  data: () => ({
-    // 
-  }),
 };
 </script>
